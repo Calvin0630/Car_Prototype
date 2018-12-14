0b8f18018eb9efbc30a385a1cc9721fd6d2e4636
Calvin0630 <cg7057705@gmail.com>
2018-12-14 14:35:57 -0500

    OwO

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour {
     public float maxSpeed;
     public float engineForce;
     Rigidbody rBody;
+    public float carLength;
 	// Use this for initialization
 	void Start () {
         rBody = gameObject.GetComponent<Rigidbody>();
@@ -14,20 +15,20 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 transformRight = -Vector3.Cross(transform.up, -transform.forward);
-		Vector3 wheelDirection = transform.forward + (0.2f*Input.GetAxis("Left_Stick_X")*transformRight);
-		
-		float rightTrigger = Input.GetAxis("Right_Trigger");
-		Debug.Log("X: " + Input.GetAxis("Left_Stick_X") + "  Y: " + Input.GetAxis("Left_Stick_Y"));
+		Vector3 wheelDirection = transform.forward + 0.75f*(Input.GetAxis("Left_Stick_X")*transformRight);
+        float deltaDistance;
+
+        float rightTrigger = Input.GetAxis("Right_Trigger");
 		if (rBody.velocity.magnitude<maxSpeed&&rightTrigger>0.5f) {
 				rBody.AddForce(rightTrigger * wheelDirection* engineForce*Mathf.Log(-rBody.velocity.magnitude+maxSpeed));
-				float deltaDistance = Time.deltaTime*rBody.velocity.magnitude;
 		}
 		float leftTrigger = Input.GetAxis("Left_Trigger");
 		if (rBody.velocity.magnitude<maxSpeed&&leftTrigger>0.5f) {
 				rBody.AddForce(-leftTrigger * wheelDirection* engineForce*Mathf.Log(-rBody.velocity.magnitude+maxSpeed));
-				float deltaDistance = Time.deltaTime*rBody.velocity.magnitude;
-		}
-	}
-	transform.forward = Vector3.Lerp(transform.forward, wheelDirection, rBody.velocity/maxSpeed);
+        }
+        deltaDistance = Time.deltaTime * rBody.velocity.magnitude;
+
+        transform.forward = transform.forward+(rBody.velocity.magnitude/maxSpeed)*Time.deltaTime*wheelDirection;
+    }
 	
 }
