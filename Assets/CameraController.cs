b89bbcde325117a7543259b0c4fe808c365736a3
Calvin0630 <cg7057705@gmail.com>
2018-12-14 13:46:07 -0500

    UwU

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public GameObject target;
+	float height =45;
+	float trailingDistance = 80;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 deltaPos = trailingDistance*-target.transform.forward + height*target.transform.up;
+		transform.position =target.transform.position + deltaPos;
+		transform.LookAt(target.transform);
+	}
+}
---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float maxSpeed;
+    public float engineForce;
+    Rigidbody rBody;
+	// Use this for initialization
+	void Start () {
+        rBody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 transformRight = -Vector3.Cross(transform.up, -transform.forward);
+		Vector3 wheelDirection = transform.forward + (0.2f*Input.GetAxis("Left_Stick_X")*transformRight);
+		
+		float rightTrigger = Input.GetAxis("Right_Trigger");
+		Debug.Log("X: " + Input.GetAxis("Left_Stick_X") + "  Y: " + Input.GetAxis("Left_Stick_Y"));
+		if (rBody.velocity.magnitude<maxSpeed&&rightTrigger>0.5f) {
+				rBody.AddForce(rightTrigger * wheelDirection* engineForce*Mathf.Log(-rBody.velocity.magnitude+maxSpeed));
+				float deltaDistance = Time.deltaTime*rBody.velocity.magnitude;
+		}
+		float leftTrigger = Input.GetAxis("Left_Trigger");
+		if (rBody.velocity.magnitude<maxSpeed&&leftTrigger>0.5f) {
+				rBody.AddForce(-leftTrigger * wheelDirection* engineForce*Mathf.Log(-rBody.velocity.magnitude+maxSpeed));
+				float deltaDistance = Time.deltaTime*rBody.velocity.magnitude;
+		}
+	}
+	transform.forward = Vector3.Lerp(transform.forward, wheelDirection, rBody.velocity/maxSpeed);
+	
+}
